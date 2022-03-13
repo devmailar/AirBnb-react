3fed198facc554d2d54af5dfcd3951cda3650f73
devmailar <83369389+devmailar@users.noreply.github.com>
2022-03-13 14:46:45 +0200

    update cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,21 @@
-import image from "./img/katie-zaferes.png";
 import star from "./img/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <div className="card_header">
-        <img src={image} alt="katiezaferes" className="character_img" />
+        <img src={props.img} alt="katiezaferes" className="character_img" />
       </div>
       <div className="card_info">
         <div className="rating">
           <img src={star} alt="star" className="card_star" />
-          <span>5.0</span>
+          <span>{props.rating}</span>
         </div>
         <div className="content">
-          <p>Life Lessons with Katie Zaferes</p>
-          <p>From $1366 / person</p>
+          <p>{props.title}</p>
+          <p>From ${props.price} / person</p>
         </div>
       </div>
     </div>
-
-    
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 import Article from "./Article";
 import Card from "./Card";
+
+import person1 from "./img/katie-zaferes.png";
+import person2 from "./img/katie-zaferes.png";
 
 export default function Header() {
   return (
     <div className="header">
       <Navbar />
       <Article />
-      <Card />      <Card />
-
+      <Card img={person1} rating="5.0" title="Life Lessons with Katie Zaferes" price={136} />
+      <Card img={person2} rating="5.0" title="Hello World" price={136} />
+      <Card img={person2} rating="5.0" title="Hello World" price={136} />
+      <Card img={person2} rating="5.0" title="Hello World" price={136} />
     </div>
   );
 }
